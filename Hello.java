67691d57516feda06f2fce6a95d047f7b4366d71
surveganesh <41013452+surveganesh@users.noreply.github.com>
2020-09-13 22:41:01 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+
+	public static void main(String args[]){
+		for (int i=1;i<=10;i++){
+			System.out.println("Hello World....."+i);
+			System.out.println("Thanks for the webhooks demo..");
+			System.out.println("Thanks for the webhooks demo2..");			
+			System.out.println("demo on webhooks - 03sep 2020");
+			System.out.println("demo on webhooks - sep 2020");
+		}
+	}
+	
+}
