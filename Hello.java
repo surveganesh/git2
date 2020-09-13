92935e313c03cdac41e77c908a80e88fb4599f31
surveganesh <41013452+surveganesh@users.noreply.github.com>
2020-09-13 23:16:53 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,9 @@
 public class Hello{
 
 	public static void main(String args[]){
-		for (int i=1;i<=10;i++){
+		for (int i=1;i<=3;i++){
 			System.out.println("Hello World....."+i);
-			System.out.println("Thanks for the webhooks demo..");
-			System.out.println("Thanks for the webhooks demo2..");			
-			System.out.println("demo on webhooks - 03sep 2020");
-			System.out.println("demo on webhooks - sep 2020");
+			System.out.println("Thanks for the webhooks demo..13th sept");
 		}
 	}
 	
